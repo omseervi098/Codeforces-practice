29b292a8d029bc6916099fe2258137cb92614802
Omprakash Choudhary <omseervi098@gmail.com>
2022-10-25 21:10:12 +0530

    update

---
+++ 734A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii;  
+typedef priority_queue <int, vector<int>, greater<int> > minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size()) 
+#define pb push_back 
+#define all(c) (c).begin(),(c).end() 
+#define cpresent(c,x) (find(all(c),x) != (c).end()) 
+#define present(c,x) ((c).find(x) != (c).end()) 
+#define test(t) while(t--) 
+int main(){
+   int n;cin>>n;
+   string s;cin>>s;
+   int cnta=0,cntd=0;
+   for(auto it:s){
+      if(it=='A') cnta++;
+      else cntd++;
+   }
+   if(cnta>cntd){
+      cout<<"Anton"<<endl;
+   }else if(cnta<cntd){
+      cout<<"Danik"<<endl;
+   }else {
+      cout<<"Friendship"<<endl;
+   }
+}
