3c2481b8f02d7543e438a1e9752d3c88703f8360
Omprakash Choudhary <omseervi098@gmail.com>
2022-10-29 10:32:57 +0530

    update

---
+++ 133A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef priority_queue<int, vector<int>, greater<int>> minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define present(c, x) ((c).find(x) != (c).end())
+#define test(t) while (t--)
+int main()
+{
+    string s;
+    cin >> s;
+    bool check = false;
+    for (auto it : s)
+    {
+        if (it == 'H' || it == 'Q' || it == '9')
+        {
+            check = true;
+        }
+    }
+    if (check)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
