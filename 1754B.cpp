fb036c73457dbbc900e0df3cbfd35fa8a1001567
Omprakash Choudhary <omseervi098@gmail.com>
2022-10-30 22:46:30 +0530

    update

---
+++ 1754B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef priority_queue<int, vector<int>, greater<int>> minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define present(c, x) ((c).find(x) != (c).end())
+#define test(t) while (t--)
+int main()
+{
+    int t;
+    cin >> t;
+    test(t)
+    {
+        int n;
+        cin >> n;
+        int temp = n / 2 + 1;
+        int k = 1;
+        int i = 0;
+        while (i < n)
+        {
+            cout << temp++ << " ";
+            i++;
+            if (i == n)
+                break;
+            cout << k++ << " ";
+            i++;
+        }
+        cout << endl;
+    }
+}
