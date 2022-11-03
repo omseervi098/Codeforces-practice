331c6c36f5cc03e4c2342f9afe436715f5a4e6c8
Omprakash Choudhary <omseervi098@gmail.com>
2022-11-03 05:43:01 +0530

    update

--- 1739A.cpp
+++ 1739A.cpp
@@ -1,32 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef vector<int> vi;
-typedef vector<vi> vvi; 
-typedef pair<int,int> ii;  
-typedef priority_queue <int, vector<int>, greater<int> > minhp;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef priority_queue<int, vector<int>, greater<int>> minhp;
 typedef priority_queue<int> maxhp;
 #define ll long long
 #define l long
 #define d double
-#define sz(a) int((a).size()) 
-#define pb push_back 
-#define all(c) (c).begin(),(c).end() 
-#define cpresent(c,x) (find(all(c),x) != (c).end()) 
-#define present(c,x) ((c).find(x) != (c).end()) 
-#define test(t) while(t--) 
-int main(){
-   int t;
-   cin>>t;
-   test(t){
-    int n,m;
-    cin>>n>>m;
-    if(n<=3 and m<=3){
-        if(n==3 and m==3) cout<<"2 2"<<endl;
-        else if(n==2 and m==3) cout<<"1 2"<<endl;
-        else if(n==3 and m==2) cout<<"2 1"<<endl;
-        else if(n==2 and m==2) cout<<"1 1"<<endl;
-        else if(n==1||m==1) cout<<n<<" "<<m<<endl; 
-    }else
-    cout<<n<<" "<<m<<endl;
-   }
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define present(c, x) ((c).find(x) != (c).end())
+#define test(t) while (t--)
+int main()
+{
+    int t;
+    cin >> t;
+    test(t)
+    {
+        int n, m;
+        cin >> n >> m;
+        if (n <= 3 and m <= 3)
+        {
+            if (n == 3 and m == 3)
+                cout << "2 2" << endl;
+            else if (n == 2 and m == 3)
+                cout << "1 2" << endl;
+            else if (n == 3 and m == 2)
+                cout << "2 1" << endl;
+            else if (n == 2 and m == 2)
+                cout << "1 1" << endl;
+            else if (n == 1 || m == 1)
+                cout << n << " " << m << endl;
+        }
+        else
+            cout << n << " " << m << endl;
+    }
 }
---
+++ 723A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef priority_queue<int, vector<int>, greater<int>> minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define present(c, x) ((c).find(x) != (c).end())
+#define test(t) while (t--)
+int main()
+{
+    vector<int> v(3);
+    cin >> v[0] >> v[1] >> v[2];
+    sort(v.begin(), v.end());
+    int sm = 0;
+    sm = v[1] - v[0];
+    sm += v[2] - v[1];
+    cout << sm << endl;
+}
