3f5abfde49b00f0ecc1c2dbe950ecf81d140eb8d
Omprakash Choudhary <omseervi098@gmail.com>
2022-11-20 20:32:42 +0530

    update

---
+++ 1759A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef priority_queue<int, vector<int>, greater<int>> minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define present(c, x) ((c).find(x) != (c).end())
+#define test(t) while (t--)
+int main()
+{
+    int t;
+    cin >> t;
+    test(t)
+    {
+        string s;
+        cin >> s;
+        string t = "";
+        for (int i = 0; i < 32; i++)
+        {
+            t += "Yes";
+        }
+        size_t fnd = t.find(s);
+        if (fnd != string::npos)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+}
