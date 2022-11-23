f0135c6ba33d46a170da06daea8c66540aa13b58
Omprakash Choudhary <omseervi098@gmail.com>
2022-11-23 22:08:16 +0530

    update

---
+++ 1760C.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef priority_queue<int, vector<int>, greater<int>> minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define present(c, x) ((c).find(x) != (c).end())
+#define test(t) while (t--)
+int main()
+{
+    int t;
+    cin >> t;
+    test(t)
+    {
+        int n;
+        cin >> n;
+        vector<ll> v(n);
+        for (auto &i : v)
+            cin >> i;
+        ll mx = *max_element(v.begin(), v.end());
+        for (auto i : v)
+        {
+            if (i == mx)
+                cout << 0 << " ";
+            else
+                cout << i - mx << " ";
+        }
+        cout << endl;
+    }
+}
