655bfa7b2142fdf435a471f1f71c7a2d6ab15404
Omprakash Choudhary <omseervi098@gmail.com>
2022-12-09 23:47:19 +0530

    update

---
+++ 443A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii;  
+typedef priority_queue <int, vector<int>, greater<int> > minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size()) 
+#define pb push_back 
+#define all(c) (c).begin(),(c).end() 
+#define cpresent(c,x) (find(all(c),x) != (c).end()) 
+#define present(c,x) ((c).find(x) != (c).end()) 
+#define test(t) while(t--) 
+int main(){
+   string s;
+   getline(cin,s);
+   unordered_set<char>st;
+   for(int i=1;i<s.size();i++){
+     
+    if((s[i]==','||i==s.size()-1) and (s[i-1]!=' ' and s[i-1]!='{'  ) ){
+        //cout<<s[i-1];
+        st.insert(s[i-1]);
+    }
+   }
+   cout<<st.size()<<endl;
+}
