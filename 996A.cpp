e682f0ac062734f11f67c7679ed97b9ac11d99e0
Omprakash Choudhary <omseervi098@gmail.com>
2022-12-11 23:58:31 +0530

    new file:   996A.cpp

---
+++ 996A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii;  
+typedef priority_queue <int, vector<int>, greater<int> > minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size()) 
+#define pb push_back 
+#define all(c) (c).begin(),(c).end() 
+#define cpresent(c,x) (find(all(c),x) != (c).end()) 
+#define present(c,x) ((c).find(x) != (c).end()) 
+#define test(t) while(t--) 
+int main(){
+   int n;
+   cin>>n;
+   vector<int> v={100,20,10,5,1};
+   int cnt=0;
+   for(auto it:v){
+    while(n>0 and it<=n){
+        cnt++;
+        n-=it;
+    }
+   }cout<<cnt<<endl;
+}
