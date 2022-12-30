59ec47333c3b398705abea4df8bdc63dbe3b5a89
Omprakash Choudhary <omseervi098@gmail.com>
2022-12-30 23:14:31 +0530

    update

---
+++ 472A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii;  
+typedef priority_queue <int, vector<int>, greater<int> > minhp;
+typedef priority_queue<int> maxhp;
+#define ll long long
+#define l long
+#define d double
+#define sz(a) int((a).size()) 
+#define pb push_back 
+#define all(c) (c).begin(),(c).end() 
+#define cpresent(c,x) (find(all(c),x) != (c).end()) 
+#define present(c,x) ((c).find(x) != (c).end()) 
+#define test(t) while(t--) 
+int main(){
+   int n;
+   cin>>n;
+   if(n%2==0){
+       cout<<4<<" "<<n-4;
+   }
+   else{
+       cout<<9<<" "<<n-9;
+   }
+}
